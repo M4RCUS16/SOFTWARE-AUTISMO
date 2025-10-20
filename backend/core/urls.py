a5c171e0ffe8bbe743b298ca8b88c6ef1b13f87b
M4RCUS16 <marcusfeitosa06@gmail.com>
2025-10-20 19:35:17 -0300

    Adiciona rota para servir React no Django

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,33 +1,22 @@
 """
 URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    # Painel administrativo
     path('admin/', admin.site.urls),
+
+    # Documentação da API
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+    # Endpoints da API principal
     path('api/', include('clinical.urls')),
-]
 
-from django.views.generic import TemplateView
-from django.urls import re_path
-
-urlpatterns += [
+    # 🔹 Catch-all: qualquer rota não capturada acima serve o React
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
