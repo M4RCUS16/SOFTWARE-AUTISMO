4045ca8c01eb3c04b93a7f6b357ee5139e4a9266
M4RCUS16 <marcusfeitosa06@gmail.com>
2025-10-20 11:19:31 -0300

    Correção final: paths Django no Render

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,15 +1,12 @@
 """
-ASGI config for backend.core project.
+ASGI config for core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
