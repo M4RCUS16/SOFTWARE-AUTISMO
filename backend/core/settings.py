4331c1daf814ad265fc1fcc7b0eb6e3d07b7544c
M4RCUS16 <marcusfeitosa06@gmail.com>
2025-10-20 19:19:51 -0300

    Corrige caminho de assets para React dist

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR.parent, 'frontend', 'dist'),
+    os.path.join(BASE_DIR.parent, 'frontend', 'dist', 'assets'),
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
